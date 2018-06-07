b709d2cceeedbc20f1f6bffe5f813d9ba31a7546
Leonardo Falotico <leonardo.falotico@edev.com.br>
2018-06-06 22:03:23 -0300

    Upload arquivos wordpress

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/home/charm804/public_html/allegrobuffet/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/home/charm804/public_html/allegrobuffet/wp-content/wflogs/');
+	include_once '/home/charm804/public_html/allegrobuffet/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
